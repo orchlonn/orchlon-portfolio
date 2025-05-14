6b9d68f373dd1072e0d13534a9d374004ead1a2e
orchlonn <ch.orchlon@gmail.com>
2025-05-13 19:13:20 -0700

    changed routing

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,39 +1,29 @@
+import Link from 'next/link';
+
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16 items-center">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-center h-16 items-center">
           <div className="flex space-x-4">
-            <a
-              href="#about"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+            <Link
+              href="/about"
+              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
             >
               About
-            </a>
-            <a
-              href="#projects"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+            </Link>
+            <Link
+              href="/projects"
+              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
             >
-              CV & Resume
-            </a>
-            <a
-              href="#portfolio"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Portfolio
-            </a>
-            <a
-              href="#experience"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              Projects
+            </Link>
+            <Link
+              href="/experience"
+              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
             >
               Experience
-            </a>
-            <a
-              href="#contact"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Contact
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,5 @@
 "use client";
 
-import HomeLeftSide from "@/Components/Home/HomeLeftSide";
-import { useState } from "react";
-import Experience from "./screens/Experience";
-import AboutSection from "./screens/AboutSection";
-import ProjectSection from "./screens/ProjectSection";
-import Navbar from "@/Components/Navbar";
-import Profile from "../../public/profile.jpeg";
-import Image from "next/image";
-import AboutMe from "@/Components/Home/AboutMe";
 export default function Home() {
-  const [section, setSection] = useState(1);
-
-  return (
-    <div className="h-screen w-screen">
-      <Navbar />
-      <AboutMe />
-      {/* <div className="xl:flex xl:flex-row text-white">
-        <HomeLeftSide setSection={setSection} />
-        <div className="xl:w-1/2 pb-10">
-          <div>
-            {section == 1 ? <AboutSection /> : null}
-            {section == 2 ? <Experience /> : null}
-            {section == 3 ? <ProjectSection /> : null}
-          </div>
-        </div>
-      </div> */}
-    </div>
-  );
+  return <div className="h-screen w-screen"></div>;
 }

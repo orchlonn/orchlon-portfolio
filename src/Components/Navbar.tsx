c1a99e179f4a112208f44f37685c305c4d9c65e0
orchlonn <ch.orchlon@gmail.com>
2025-05-13 21:16:08 -0700

    navbar logic implemented

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,32 +1,48 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const [section, setSection] = useState(0);
+
   return (
     <nav className="bg-gray-800 text-white">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center h-16 items-center">
           <div className="flex space-x-4">
             <Link
-              href="/about"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+              href="./"
+              onClick={() => setSection(0)}
+              className={`${
+                section === 0 ? "bg-gray-700 text-white" : "text-gray-400"
+              } hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer`}
             >
               About
             </Link>
             <Link
               href="/resume"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+              onClick={() => setSection(1)}
+              className={`${
+                section === 1 ? "bg-gray-700 text-white" : "text-gray-400"
+              } hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer`}
             >
               CV & Resume
             </Link>
             <Link
               href="/projects"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+              onClick={() => setSection(2)}
+              className={`${
+                section === 2 ? "bg-gray-700 text-white" : "text-gray-400"
+              } hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer`}
             >
               Projects
             </Link>
             <Link
+              onClick={() => setSection(3)}
               href="/experience"
-              className="hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+              className={`${
+                section === 3 ? "bg-gray-700 text-white" : "text-gray-400"
+              } hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer`}
             >
               Experience
             </Link>

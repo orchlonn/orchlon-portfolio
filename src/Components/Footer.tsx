8f13ca74d7c2d999d667610c80a906a8d2b71494
orchlonn <ch.orchlon@gmail.com>
2025-05-18 13:37:10 -0700

    added footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="text-gray-300 mx-auto">
+      <div className="flex space-x-4 justify-center items-center my-3 mt-10">
+        {/* Social Media Icons */}
+        <a
+          href="https://github.com/orchlonn"
+          target="_blank"
+          className="hover:text-highlight-orange"
+        >
+          <GitHubLogoIcon width={20} height={20} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/orchlonc/"
+          target="_blank"
+          className="hover:text-highlight-orange"
+        >
+          <LinkedInLogoIcon width={20} height={20} />
+        </a>
+      </div>
+      {/* Name and Date */}
+      <div className="flex justify-center items-center mb-10">
+        <div className="text-sm text-center md:text-right">
+          &copy; {new Date().getFullYear()} Oscar Chinbat. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

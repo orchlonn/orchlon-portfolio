bbadc83efe9a97c79dcfbf9289ba1acd8f515125
orchlonn <ch.orchlon@gmail.com>
2025-06-02 12:44:02 -0700

    added projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 import SectionHeader from "@/Components/SectionHeader";
 import SingleProject from "./Components/SingleProject";
 import BizzzyLogo from "../../../public/Bizzzy_logo.png";
+import SortingAppLogo from "../../../public/sorting_app_logo.png";
+import LogisticCompanyLogo from "../../../public/logistic_company_logo.png";
 
 const ProjectSection = () => {
   return (
@@ -14,16 +16,16 @@ const ProjectSection = () => {
           demoLink="https://drive.google.com/file/d/1wuJXLsGQBIHQUbkEfV9y3TfLk_BmVOU-/view?usp=sharing"
         />
         <SingleProject
-          title="Bizzzy - Handy human helps you"
-          image={BizzzyLogo}
-          demoLink="https://github.com/Bizzzy-software"
-          githubLink="https://github.com/Bizzzy-software"
+          title="Sorting Application - VR Sorting algorithms"
+          image={SortingAppLogo}
+          demoLink=""
+          githubLink="https://github.com/CS-466-group-4/VR-Sorting-app"
         />
         <SingleProject
-          title="Bizzzy - Handy human helps you"
-          image={BizzzyLogo}
-          demoLink="https://github.com/Bizzzy-software"
-          githubLink="https://github.com/Bizzzy-software"
+          title="Landing page - Logistic Company Website"
+          image={LogisticCompanyLogo}
+          demoLink="https://andjintrans.vercel.app/"
+          githubLink="https://github.com/orchlonn/Andjintrans"
         />
       </div>
     </div>

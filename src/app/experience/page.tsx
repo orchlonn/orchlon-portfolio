3670d7ab571c4ccc8fd19b686fcf17ab569e2abd
orchlonn <ch.orchlon@gmail.com>
2026-02-09 15:03:57 -0800

    update

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -59,37 +59,31 @@ const Experience = () => {
         />
         <ExperienceItem
           image={CWULogo}
-          position="Undergraduate Researcher | Apr 2025 - Jun 2025"
+          position="AI/ML Researcher | Apr 2025 - Jun 2025"
           company="Central Washington University"
           description={[
             <>
-              ● Conducted in-depth SQL Injection testing in a controlled test
-              environment using SQLMap and custom payloads, uncovering{" "}
-              <span className="font-bold text-white"> 20 </span>
-              critical vulnerabilities in a web-based financial application and
-              guiding patch deployments that reduced potential breach risk by
-              <span className="font-bold text-white"> 90%. </span>
+              ● Implemented <span className="font-bold text-white"> KNN </span>{" "}
+              to predict housing prices based on location and property features,
+              improving model precision by{" "}
+              <span className="font-bold text-white"> 12%. </span>
             </>,
             <>
-              ● Conducted network reconnaissance and port scanning in a
-              virtualized test environment with Nmap and Zenmap across{" "}
-              <span className="font-bold text-white"> 100+ </span>
-              hosts, mapping <span className="font-bold text-white"> 250 </span>
-              open ports and service configurations—resulting in tightened
-              firewall rules and a{" "}
-              <span className="font-bold text-white"> 70% </span> reduction in
-              exposed attack surface.
+              ● Applied <span className="font-bold text-white"> KNN </span> to
+              classify handwritten digits from image data, achieving{" "}
+              <span className="font-bold text-white"> 95% </span> prediction
+              accuracy.
             </>,
           ]}
           skills={[
-            "Higher Education Research",
-            "Cybersecurity",
-            "Software Infrastructure",
-            "MySQL",
-            "SQL Injection",
-            "Script Injection",
-            "Penetration Testing",
-            "Port Scanning",
+            "PyTorch",
+            "Hugging Face",
+            "NLP",
+            "LLMs",
+            "RAG",
+            "Jupyter",
+            "Research",
+            "Fine-tuning",
           ]}
           initialSkillCount={4}
           className="animate-slide-in-delay w-full sm:w-4/5 lg:w-2/5"
